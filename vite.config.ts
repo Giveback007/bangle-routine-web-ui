dcadffa578cc5a5550d754097d9d99db934770b3
Dovy <dovy91@gmail.com>
2025-11-28 23:24:23 -0500

    potential fix for build failure

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 	plugins: [
